7859927e9d7661f222db2a03b76fc358adbf3845
papo <pablo.huichalaf.prisdai2022@gmail.com>
2023-02-04 22:44:23 -0300

    primera versión del programa que envía el qr al residente o administrador

--- mailServer/main.py
+++ mailServer/main.py
@@ -4,24 +4,27 @@ import mimetypes
 
 from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
-#rom email.Encod import encode_base64
+
 
 def send_mail(destinatario, subject, file_name):
+
     remitente = "habitat.segura.chile@gmail.com"
-    mensaje = "¡Hola, mundo!"
 
     email = MIMEMultipart()
     email["From"] = remitente
     email["To"] = destinatario
     email["Subject"] = subject
+    
     file = open(file_name, "rb")
     attach_image = MIMEImage(file.read())
     attach_image.add_header('Content-Disposition', f'attachment; filename = {file_name}')
     email.attach(attach_image)
 
-    with open('token', 'r') as f:
+    with open('mailServer/token', 'r') as f:
         token = str(f.read())
+    
     smtp = smtplib.SMTP_SSL("smtp.gmail.com")
     smtp.login(remitente, token)
     smtp.sendmail(remitente, destinatario, email.as_string())
     smtp.quit()
+    print(f"email enviado desde {remitente}, hacia {destinatario}, asunto {subject}, archivo {file_name}")
